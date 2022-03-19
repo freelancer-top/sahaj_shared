77978697d773d5e19d8d1c594cfba469014ca604
freelancer-top <101926383+freelancer-top@users.noreply.github.com>
2022-03-19 15:35:47 +0530

    Create aspnet.groovy

---
+++ vars/aspnet.groovy
@@ -0,0 +1,31 @@
+def call(String param = 'test') {
+ pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'                
+
+            bat """                
+                dir
+                msbuild SahajPayroll.sln
+              """                 
+                
+                
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+    }
+}
+  
+}
