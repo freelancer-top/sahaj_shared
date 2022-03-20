203fecbcfcd8cae8c4feb39b355b803e83b62010
freelancer-top <101926383+freelancer-top@users.noreply.github.com>
2022-03-20 11:11:34 +0530

    Update aspnet.groovy

--- vars/aspnet.groovy
+++ vars/aspnet.groovy
@@ -1,7 +1,29 @@
 def call(String param = 'test') {
  pipeline {
     agent any
-
+parameters([
+                            choice(
+                                choices: ['ONE', 'TWO'], 
+                                name: 'PARAMETER_01'
+                            ),
+                            booleanParam(
+                                defaultValue: true, 
+                                description: '', 
+                                name: 'BOOLEAN'
+                            ),
+                            text(
+                                defaultValue: '''
+                                this is a multi-line 
+                                string parameter example
+                                ''', 
+                                 name: 'MULTI-LINE-STRING'
+                            ),
+                            string(
+                                defaultValue: 'scriptcrunch', 
+                                name: 'STRING-PARAMETER', 
+                                trim: true
+                            )
+                        ])
     stages {
         stage('Build') {
             steps {
