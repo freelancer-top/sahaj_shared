a191a7003ffa4b96d599010c65253000a830dd65
freelancer-top <101926383+freelancer-top@users.noreply.github.com>
2022-03-21 12:41:27 +0530

    Update aspnet.groovy

--- vars/aspnet.groovy
+++ vars/aspnet.groovy
@@ -20,14 +20,7 @@ def call(String param = 'test') {
         }
         stage('Test') {
             steps {
-                echo 'Testing..'
-                def msbuildHome = tool 'Default MSBuild'
-                def scannerHome = tool 'SonarScanner for MSBuild'
-        withSonarQubeEnv() {
-      bat "\"${scannerHome}\\SonarScanner.MSBuild.exe\" begin /k:\"SahajVikas_SahajPayroll\""
-      bat "\"${msbuildHome}\\MSBuild.exe\" /t:Rebuild"
-      bat "\"${scannerHome}\\SonarScanner.MSBuild.exe\" end"
-        } 
+                echo 'Testing..'             
              
             }
         }
