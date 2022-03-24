d56e381dd759c21324b4f9d7f8de603297954a8d
vikas-india-panditsharma <80153429+vikas-india-panditsharma@users.noreply.github.com>
2022-03-24 18:28:35 +0530

    ts

--- vars/helper.groovy
+++ vars/helper.groovy
@@ -13,7 +13,8 @@ def buildAspNet(buildParam)
                 @echo off
                 dir
                 nuget.exe restore ${buildParam.jenkinParams.solutionFile}
-                msbuild /t:Rebuild /t:restore  /property:Configuration=Release                
+                msbuild /t:Rebuild /t:restore  /property:Configuration=Release 
+                msbuild /property:Configuration=Release
               """     
 } 
 def runQualityGate(buildParam)
