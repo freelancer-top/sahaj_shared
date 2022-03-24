552eec6cf8b9502ff123bf0a54e904bf2edc266b
vikas-india-panditsharma <80153429+vikas-india-panditsharma@users.noreply.github.com>
2022-03-24 18:56:10 +0530

    test

--- vars/helper.groovy
+++ vars/helper.groovy
@@ -31,8 +31,8 @@ def deloyCode(buildParam)
 //@echo off
         bat """                
                 
-                rmdir /s /q "${buildParam.jenkinParams.build.outApp.deployFolder}/*"
-                Xcopy ${buildParam.jenkinParams.build.outApp}/bin "${buildParam.jenkinParams.build.outApp.deployFolder}" /E /H /C /I
+                rmdir /s /q "${buildParam.jenkinParams.build.deployFolder}/*"
+                Xcopy ${buildParam.jenkinParams.build.outApp}/bin "${buildParam.jenkinParams.build.deployFolder}" /E /H /C /I
 
               """  
 
